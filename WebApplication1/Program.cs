548426948ea2da869ad56d742b910165817d75d3
vlad.irimia <vlad.irimia@ifacts.se>
2023-08-02 16:24:31 +0300

    initial commit

---
+++ WebApplication1/Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using SixMinApi.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var sqlConnectionBuilder = new SqlConnectionStringBuilder();
+sqlConnectionBuilder.ConnectionString = builder.Configuration.GetConnectionString("SQLDbConnection");
+sqlConnectionBuilder.UserID = builder.Configuration["UserId"];
+sqlConnectionBuilder.Password = builder.Configuration["Password"];
+
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseSqlServer(sqlConnectionBuilder.ConnectionString);
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+// request here
+
+app.Run();
